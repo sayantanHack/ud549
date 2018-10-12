aabb70be00264e27ab1d506900adf95d4c749d64
sayantanHack <sayantandatta97@gmail.com>
2018-10-12 13:21:51 +0530

    few changes on the addressbok

--- spec/AddressBookSpec.js
+++ spec/AddressBookSpec.js
@@ -7,4 +7,17 @@ describe('Address Book', function(){
 
       expect(addressBook.getContact(0)).toBe(thisContact)
   });
+
+// Deleting the contacts
+  it('should be able to delete a contact', function(){
+    var addressBook = new AddressBook(),
+    thisContact = new Contact();
+
+    addressBook.addContact(thisContact);
+    addressBook.deleteContact(0);
+
+    // expect is here used for if element are deleted then it will no longer show it.
+    expect(addressBook.getContact(0)).not.toBeDefined();
+  });
+
 });
